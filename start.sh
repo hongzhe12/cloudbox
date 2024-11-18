86d83802e0e52212b4b0aba8b841b558acd1ffe6
hzml <hongzhe2022@163.com>
2024-11-18 23:47:45 +0800

    update: 使用uvicorn作为服务器

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 # 收集静态文件
 python manage.py collectstatic --noinput
 
-# 生成新的迁移文件
+# 检测模型变化
 python manage.py makemigrations
 
-# 创建数据库和应用迁移
+# 数据库迁移
 python manage.py migrate
 
 # 启动 Django 开发服务器
-uvicorn PureBlog.asgi:application --host 0.0.0.0 --port 8000 --workers 10
+uvicorn CloudBox.asgi:application --host 0.0.0.0 --port 8000 --workers 10
