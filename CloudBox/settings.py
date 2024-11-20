cce7ac6519f1c73310bd95aac2e7ccd7243d1c9e
hzml <hongzhe2022@163.com>
2024-11-20 14:21:58 +0800

    update: 修复数据卷挂载

--- CloudBox/settings.py
+++ CloudBox/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'CloudBox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cloudbox',
+        'USER': 'cloudbox',
+        'PASSWORD': 'password',
+        'HOST': 'db',  # Docker Compose 中 Postgres 服务的名称
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
